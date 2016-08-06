351b87f5b2ba26222805ce16a643c1ab18a859d1
Thys Rabe <thys.rabe@gmail.com>
2016-08-06 12:22:20 +0200

    Added new file filelist5.py

---
+++ filelist5.py
@@ -0,0 +1,9 @@
+import os, glob
+with open ("file_size.txt", "w") as outfile:
+    files= glob.glob ("/Volumes/Video/*/*/*.avi")
+    for file in files:
+        size = os.path.getsize(file)
+        if size < 2500000000:
+            msize = str (round ((size/(1024*1024)),1)) + "M"
+        out_name = os.path.basename(file)
+        outfile.write(out_name + " , " + str(msize) + "\n")
